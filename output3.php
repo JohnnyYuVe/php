015b7f08553627635d25414d7ea4ac78ecbb3624
John Veneth yu <elite4ever7@gmail.com>
2024-05-29 22:27:55 +0200

    esercitazioni su php

--- output3.php
+++ output3.php
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 
+
 <!DOCTYPE html
 PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -16,16 +17,78 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	
 	<body>
 
-			<h1>	</h1>
-
-			<p>
-
-			</p>
+		<h1 style="text-align:center;">ecco il risultato dei dati da te inseriti</h1>
 
 
 
+		<?php
+//Controllo le variabili nome e cognome per vedere se è stato inserito qualcosa siccome un blank e e considerato un carattere
+				if($_POST["nome"]==" " || $_POST["cognome"]==" "){
+									echo "<p>hey non mi hai detto il tuo nome e il tuo cognome</p>";
+				}
+				else{
+					echo "<p> Ciao".$_POST["nome"]."-". $_POST["cognome"] .", ti ringrazio di aver compilato il form!</p>";
+				}
+			
+				echo "<p>vediamo un po' quali sono i tuoi gusti...</p>";
 
 
+//Controllo della variabile Genere dentro l'array di default _POST 
+				if(!isset($_POST["Genere"])){
+											echo "<p>Non hai scelto nessun genere letterario</p>";
+				}
+				
+				else{
+					switch ($_POST["Genere"]) {
+						
+						case 'Giallo':
+									echo "<p>ti piace il veramente ilGiallo? che noioso...</p>";
+						break;
+						
+						case 'Fantascienza':
+										echo "<p>ti piace la Fantascienza Wow</p>";
+						break;
+						
+						case 'Horror':
+									echo "<p>ti piace l'horror, Coraggioso!</p>";
+						break;			
+					}
+				}
+
+//Controllo della variabile PrefSquadra dentro l'array di default _POST e vedo quali fiori ha scelto 
+				if(!isset($_POST["PrefSqadra"])){
+												echo "<p>hey non hai scelto nessuna squadra</p>";
+				}
+				else{
+					echo"<h1>Hai scelto le seguenti squadre</h1>";
+					echo"<p>";
+						foreach ($_POST["PrefSqadra"] as $Pos => $value) {
+								
+							echo "<li>" . $Pos . "-" . $value ."</li>";
+
+						}
+					echo"</p>";	
+
+				}
+
+
+				if(!isset($_POST["fiori"])){
+												echo "<p>hey non hai scelto nessun fiore!</p>";
+				}
+				else{
+					echo"<h1>Hai scelto i seguenti fiori</h1>";
+					echo"<p>";
+						foreach ($_POST["fiori"] as $Pos => $value) {
+								
+							echo "<li>" . $Pos . "-" . $value. "</li>";
+
+						}
+					echo"</p>";	
+
+				}
+
+
+		?>
 
 
 
